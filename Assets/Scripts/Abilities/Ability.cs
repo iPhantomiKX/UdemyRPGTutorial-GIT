97ad3a235a0d247aeaeb564abb04fad8335ecae6
iPhantomiKX <cmpsun98@gmail.com>
2020-06-13 16:43:06 +0800

    Updates:
    - Created Ability System, not the abilities themselves just yet
      - Used the ActionItem as parent
      - Ability Class is now a base class
      - Future ability classes will derive from AbilityBase Class
    
    - Things to do next:
      - Add some requirements to use ability
    
    - Amirul 13/06/2020

---
+++ Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Combat;
+
+namespace RPG.Abilities
+{
+    public class Ability : MonoBehaviour
+    {
+        public virtual void Activate()
+        {   
+            print(this.name + " is activated");
+        }
+    }
+}
+
---
+++ Assets/Scripts/Abilities/AbilityItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Abilities;
+
+namespace RPG.Inventories
+{
+    [CreateAssetMenu(menuName = ("RPG/RPG.UI.InventorySystem/Ability Item"))]
+    public class AbilityItem : ActionItem
+    {
+        [SerializeField] Ability abilitySpell;
+
+        /// <summary>
+        /// Trigger the use of this item. Override to provide functionality.
+        /// </summary>
+        /// <param name="user">The character that is using this action.</param>
+        public override void Use(GameObject user)
+        {
+            base.Use(user);
+            abilitySpell.Activate();
+        }
+    }
+}
+
